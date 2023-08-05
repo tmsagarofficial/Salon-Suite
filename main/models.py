eb69a667545d69de4e19b0956e7ddb19390eec8e
S A G A R <tmsagarofficial@gmail.com>
2023-08-05 19:34:22 +0530

    added categories page

--- main/models.py
+++ main/models.py
@@ -11,7 +11,7 @@ class Banner(models.Model):
 #Category
 class Category(models.Model):
     title=models.CharField(max_length=100)
-    image=models.ImageField(upload_to="cat_imgs/")
+    image=models.ImageField(upload_to="cat_imgs/",max_length=300)
     
     def __str__(self):
         return self.title
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Category
+#Home page
+def home(request):
+    
+    return render(request,'index.html')
 
-# Create your views here.
+def category_list(request):
+    data=Category.objects.all().order_by('-id')
+    return render(request,'category_list.html',{'data':data})
